da109a13f84ca2001e41cfbf32c7974ac08806ea
lahiru sanjana <lahirusanjana1@gmail.com>
2023-08-22 23:40:36 +0530

    finalize and swith to samitha

--- server/controllers/Book_add_author.js
+++ server/controllers/Book_add_author.js
@@ -3,7 +3,7 @@ const path = require('path');
 const Book = require('../models/Book');
 
 
-const add_book_sell = async (req,res) => {
+const add_book_author = async (req,res) => {
     const storage =multer.diskStorage({
         destination: (req, file, cb) =>{
             cb(null, 'D:/booknet_v001/backend/Backend/server/src/img')
@@ -24,13 +24,13 @@ const add_book_sell = async (req,res) => {
             return res.status(400).json({ message: 'Error uploading file', error: err });
         }
 
-        
+        // console.log("HI");
         Book.create({
             title: req.body.title,
             authors: req.body.author,
+            quantity: req.body.quantity,
             description: req.body.description,
             isbn: req.body.isbn,
-            condition: req.body.condition,
             price: req.body.price,
             img: req.file.filename })
         .then(result => res.redirect('http://localhost:3000/'))
@@ -38,4 +38,4 @@ const add_book_sell = async (req,res) => {
     });
 }
 
-module.exports = add_book_sell;
+module.exports = add_book_author;
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,7 +6,8 @@ const multer = require('multer');
 const path = require('path');
 const { signupUser, loginUser } = require("../controllers/authController");
 const addBookAuthor = require("../controllers/addBookAuthor");
-const add_book_sell = require("../controllers/addBookForSell")
+const add_book_sell = require("../controllers/addBookForSell");
+const add_book_author = require("../controllers/Book_add_author");
 
 // middleware
 router.use(
@@ -21,5 +22,6 @@ router.post("/upload", testk);
 router.post("/login", loginUser);
 router.post("/add_book",addBookAuthor);
 router.post("/add_book_detail_sell",add_book_sell)
+router.post("/Book_add_author",add_book_author)
 
 module.exports = router;
