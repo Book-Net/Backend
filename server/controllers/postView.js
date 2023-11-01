7affe2e775c794424e1984b9239feaa535707c2e
Samitha <rathnayakesamitha1317@gmail.com>
2023-11-01 10:01:29 +0530

    post create and view part

---
+++ server/controllers/postView.js
@@ -0,0 +1,14 @@
+const Post = require("../models/Post")
+
+const postView = async (req, res) => {
+    try {
+      const posts = await Post.find();
+      res.json(posts);
+    } catch (error) {
+        
+      console.error(error);
+      res.status(500).json({ error: 'An error occurred in viewing posts' });
+    }
+  };
+
+  module.exports = postView
